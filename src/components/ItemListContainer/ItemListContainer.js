e4c85a4298091d96147e650639e6884a8a4d94f0
toleZ <toloyjc@gmail.com>
2022-09-05 14:59:38 -0300

    Getting better syntax

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,31 +5,24 @@ import { useParams } from "react-router-dom";
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
 
-  const {brandName} = useParams()
+  const { brandName } = useParams();
 
   useEffect(() => {
-    if(brandName !== undefined){
-      setTimeout(() => {
-        fetch("https://63064480dde73c0f8457299d.mockapi.io/sneakers")
-          .then((res) => res.json())
-          .then((data) => setItems(data.filter(e => e.brand === brandName)))
-          .catch((error) => console.warn(error));
-      }, 2000)
-    }else{
-      setTimeout(() => {
-        fetch("https://63064480dde73c0f8457299d.mockapi.io/sneakers")
-          .then((res) => res.json())
-          .then((data) => setItems(data))
-          .catch((error) => console.warn(error));
-      }, 2000)
-    }
+    setTimeout(() => {
+      fetch("https://63064480dde73c0f8457299d.mockapi.io/sneakers")
+        .then((res) => res.json())
+        .then((data) => {
+          brandName !== undefined ? setItems(data.filter((e) => e.brand === brandName)) : setItems(data)
+        })
+        .catch((error) => console.warn(error));
+    }, 2000);
   }, [brandName]);
 
   return (
     <>
       <ItemList items={items} />
     </>
-  )
+  );
 };
 
 export default ItemListContainer;
