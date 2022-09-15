a26b8c2b1748daf3872d6569a7bdaa633f7a13ab
toleZ <toloyjc@gmail.com>
2022-09-15 00:11:16 -0300

    ItemDetail improvements

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,6 +1,6 @@
 import styles from './ItemDetail.module.css'
 import ItemCount from "../ItemCount/ItemCount";
-import { useState, useContext } from 'react';
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/CartContext';
 
@@ -8,13 +8,12 @@ const ItemDetail = ( { item } ) => {
     const {name, brand, description, img, price, stock} = item
     const {itemDetailContainer, itemImg, itemTitle, itemBrand, itemDescription, itemPrice, goCartBtn, link} = styles
 
-    const {addToCart} = useContext(CartContext)
+    const {isInCart, addToCart} = useContext(CartContext)
 
-    const [cant, setCant] = useState(0)
     const onAdd = (cnt) => {
         return cnt === 0 ? 
             console.error(`${cnt} no es una cantidad validad`) :
-            (setCant(cnt), addToCart(item, cnt))
+            addToCart(item, cnt)
     };
 
     return (
@@ -26,7 +25,7 @@ const ItemDetail = ( { item } ) => {
             <p className={itemDescription}>{description}</p>
             <span className={itemPrice}>${price}</span>
             {
-                cant > 0 ?
+                isInCart(item.id) === true ?
                     <Link to='/cart' className={link}><button className={goCartBtn}>Ir al carrito</button></Link>:
                     <ItemCount initial={0} stock={stock} onAdd={onAdd}/>
             }
