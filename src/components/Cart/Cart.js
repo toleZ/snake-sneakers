e2ca6209aad90817072ac82f99ef18658d2b7a12
toleZ <toloyjc@gmail.com>
2022-09-15 11:33:27 -0300

    Cart items styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,23 @@
 //import Form from "../Form/Form"
 import { useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
+import styles from './Cart.module.css'
 
 const Cart = () => {
     const {cart} = useContext(CartContext)
+    
+    const {itemCard, itemInfo, itemName, itemBrand, itemImg} = styles
 
     return (
         <main>
             <h1>Cart</h1>
             {cart.map(item =>
-                <div key={item.id}>
-                    <h3>{item.name}</h3>
+                <div key={item.id} className={itemCard}>
+                    <img src={item.img} alt={item.name} className={itemImg}/>
+                    <span className={itemInfo}>
+                        <h3 className={itemName}>{item.name}</h3>
+                        <h4 className={itemBrand}>{item.brand}</h4>
+                    </span>
                 </div>
             )}
         </main>
