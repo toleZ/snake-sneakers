8ac0799ad0ead787037b8fae530fb796d9c244f6
toleZ <toloyjc@gmail.com>
2022-09-15 00:19:24 -0300

    Ussing CartContext in CartComponent to import items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,19 @@
-import Form from "../Form/Form"
+//import Form from "../Form/Form"
+import { useContext } from 'react';
+import { CartContext } from '../../context/CartContext';
 
 const Cart = () => {
+    const {cart} = useContext(CartContext)
+
     return (
-        <>
+        <main>
             <h1>Cart</h1>
-            <Form />
-        </>
+            {cart.map(item =>
+                <div key={item.id}>
+                    <h3>{item.name}</h3>
+                </div>
+            )}
+        </main>
     )
 }
 
