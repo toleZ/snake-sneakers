934dbf6a1d58b82217148e9d9714932eb2a87085
toleZ <toloyjc@gmail.com>
2022-09-19 13:48:18 -0300

    Improvements of code reading

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,37 +1,30 @@
 import { createContext, useState } from "react";
 
-export const CartContext = createContext()
+export const CartContext = createContext();
 
-const CartProvider = ({children}) => {
-  const [cart, setCart] = useState([])
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
 
   const isInCart = (id) => {
-    return cart.some(item => item.id === id)
-  }
+    return cart.some((item) => item.id === id);
+  };
 
   const addToCart = (item, cant) => {
-    if(isInCart(item.id)){
-      updateItemCant(item, cant)
-    }else{
-      cant > 0 && setCart([...cart, {...item, cant}])
+    if (isInCart(item.id)) {
+      updateItemCant(item, cant);
+    } else {
+      cant > 0 && setCart([...cart, { ...item, cant }]);
     }
-  }
+  };
 
   const updateItemCant = (item, cant) => {
     const updateProducts = cart.map((prod) => {
-        if (prod.id === item.id) {
-            const productUpdated = {
-                ...prod,
-                cant: cant,
-            };
-
-            return productUpdated;
-        } else {
-            return prod;
-        }
-    });
+      return prod.id === item.id ?
+        {...prod, cant: cant,} :
+        prod
+      });
     setCart(updateProducts);
-};
+  };
 
   const removeItem = (itemId) => {
     /* 
@@ -40,28 +33,39 @@ const CartProvider = ({children}) => {
     setCart(cartCopy) 
     */
 
-    const cartFiltered = cart.filter(e => e.id !== itemId)
-    setCart(cartFiltered)
-  }
+    const cartFiltered = cart.filter((e) => e.id !== itemId);
+    setCart(cartFiltered);
+  };
 
   const clear = () => {
-    setCart([])
-  }
+    setCart([]);
+  };
 
   const cartItems = () => {
-    return [...cart].reduce((acc, cur) => acc + cur.cant, 0)
-  }
+    return [...cart].reduce((acc, cur) => acc + cur.cant, 0);
+  };
 
   const cantOfItem = (itemId) => {
-    const item = [...cart].find(e => e.id === itemId)
-    return item?.cant
-  }
+    const item = [...cart].find((e) => e.id === itemId);
+    return item?.cant;
+  };
 
   return (
-    <CartContext.Provider value={{cart, addToCart, removeItem, clear, isInCart, cartItems, cantOfItem, updateItemCant}}>
+    <CartContext.Provider
+      value={{
+        cart,
+        addToCart,
+        removeItem,
+        clear,
+        isInCart,
+        cartItems,
+        cantOfItem,
+        updateItemCant,
+      }}
+    >
       {children}
     </CartContext.Provider>
-  )
-}
+  );
+};
 
-export default CartProvider
+export default CartProvider;
