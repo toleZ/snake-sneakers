2f54f38abbdcb7d9fdb3409ab5b35502264aa2cc
toleZ <toloyjc@gmail.com>
2022-09-22 14:16:22 -0300

    Cart items styles

--- src/components/CartDetail/CartDetail.js
+++ src/components/CartDetail/CartDetail.js
@@ -5,23 +5,23 @@ import { CartContext } from "../../context/CartContext";
 
 const CartDetail = ({ item }) => {
   const { removeItem } = useContext(CartContext);
-  const { itemCard, itemInfo, itemName, itemBrand, itemImg, btn } = styles;
+  const { itemDetail, itemCard, itemInfo, itemName, itemBrand, itemImg, btn } = styles;
   const {id, name, brand, img, price, cant} = item
 
   return (
     <div className={itemCard}>
       <div style={{ display: "flex" }}>
         <img src={img} alt={name} className={itemImg} />
-        <span className={itemInfo}>
+        <div className={itemDetail}>
           <h3 className={itemName}>{name}</h3>
           <h4 className={itemBrand}>{brand}</h4>
-        </span>
+        </div>
 
-        <span>
-          Cantidad: {cant} |
-          Precio: ${price} |
-          Total: ${price * cant}
-        </span>
+        <div className={itemInfo}>
+          <span>Cantidad: {cant}u</span>
+          <span>Precio: ${price}/u</span>
+          <span>Total: ${price * cant}</span>
+        </div>
       </div>
       <button
         className={btn}
