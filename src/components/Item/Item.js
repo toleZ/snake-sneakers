2d58eb438b3d28cd067733cf4f70fc10029b8bad
toleZ <toloyjc@gmail.com>
2022-09-27 11:11:40 -0300

    Items styles

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,20 +1,30 @@
-import styles from './Item.module.css'
-import { Link } from 'react-router-dom';
-import { AiOutlineRight } from 'react-icons/ai'
+import styles from "./Item.module.css";
+import { Link } from "react-router-dom";
+import { AiOutlineRight } from "react-icons/ai";
 
-const Item = ({item}) => {
-  const {item__card, item__img, item__title, item__brand, item__price, viewMore__btn} = styles
-  const {name, brand, img, price, id} = item
+const Item = ({ item }) => {
+  const {
+    item__card,
+    item__img,
+    item__title,
+    item__brand,
+    item__price,
+    viewMore__btn,
+  } = styles;
+  const { name, brand, img, price, id } = item;
 
   return (
     <div id={id} className={item__card}>
       <img className={item__img} src={img} alt={name}></img>
-      <h2 className={item__title}>{name}</h2>
       <h3 className={item__brand}>{brand}</h3>
-      <span className={item__price}>${price}</span>
+      <h2 className={item__title}>{name}</h2>
+      <span className={item__price}>
+        desde <span style={{ color: "black" }}>${price}</span>
+      </span>
       <Link to={`/sneaker/${id}`}>
         <button className={viewMore__btn}>
-          Ver mas<AiOutlineRight />
+          Ver mas
+          <AiOutlineRight />
         </button>
       </Link>
     </div>
