4cf9d523ec1c5858db41e19faa6b30b76d971b42
toleZ <toloyjc@gmail.com>
2022-10-11 20:49:12 -0300

    .gitignore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB26jzGFuEaI2_Jg75wCjDDJGoJFOAJhPg",
-  authDomain: "sneak-sneakers.firebaseapp.com",
-  projectId: "sneak-sneakers",
-  storageBucket: "sneak-sneakers.appspot.com",
-  messagingSenderId: "711118158981",
-  appId: "1:711118158981:web:de0eec462bedf9182fbdec"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
